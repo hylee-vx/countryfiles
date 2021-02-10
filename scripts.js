eaf20d4c33cd0ca389fd1da19761278e468712e8
Hyun Young Lee <hyunyounglee@Hyuns-MacBook-Pro.local>
2021-02-10 12:31:21 +0000

    Fetch country data from API, display country name and image on button
    - link to API to replace repo test data in file
    - create loadApiList function to fetch country data - name, region, capital, languages and flag image src url - and log to console
    - create buttonContainer div to hold countryName and countryImage elements, and append both to button
    - old code commented out at bottom
    - add flexbox to css file to improve buttonContainer display; minor cosmetic changes

--- scripts.js
+++ scripts.js
@@ -1,99 +1,61 @@
-const repo = [
-    { name: 'Sweden', region: 'Europe', languages: 'Swedish' },
-    { name: 'Vanuatu', region: 'Oceania', languages: ['Bislama', 'English', 'French'] },
-    { name: 'Samoa', region: 'Oceania', languages: ['Samoan', 'English'] },
-    { name: 'Bhutan', region: 'Asia', languages: 'Dzongkha' },
-    { name: 'Martinique', region: 'Americas', languages: 'French' },
-    { name: 'Bermuda', region: 'Americas', languages: 'English' }
-]
+const worldList = [];
 
 //API
-//const url = 'https://restcountries.eu/rest/v2/';
+const apiUrl = 'https://restcountries.eu/rest/v2/';
 
-const repoCategories = ['name', 'region', 'languages'];
+const add = newCountry => worldList.push(newCountry);
 
-// checks categories of new country match categories in repo
-const checkCategories = function (newCountry) {
-    let result = true;
-    Object.keys(newCountry).forEach(function (category) {
-        if (!repoCategories.includes(category)) {
-            console.log(`${newCountry.name} has the invalid category '${category}'`);
-            result = false;
-        }
-    });
-    return result;
-};
+function loadApiList() {
+    return fetch(apiUrl).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        data.forEach(function (country) {
+            const languageArray = [];
+            const createLanguageArray = country.languages.map(function (language) {
+                languageArray.push(language.name);
+                return languageArray;
+            });
 
-// checks new country is a valid object and has the same number of categories as the countries in repo, then moves to checkCategories function
-const validateNewCountry = function (newCountry) {
-    if (typeof newCountry === 'object' && newCountry !== null &&
-        Object.keys(newCountry).length === repoCategories.length) {
-        return checkCategories(newCountry);
-    } else {
-        console.log(`${JSON.stringify(newCountry)} is not a valid object or does not have the correct categories`);
-        return false;
-    }
-};
+            const nation = {
+                name: country.name,
+                region: country.region,
+                capital: country.capital,
+                languages: languageArray, // why can't I call createLanguageArray here? returns array of index values, not language names
+                flag: country.flag
+            };
 
-// adds new country to repo if it passes validation checks
-const add = function (newCountry) {
-    if (validateNewCountry(newCountry)) {
-        return repo.push(newCountry);
-    } else {
-        console.log(`${newCountry.name || newCountry} has not been added`);
-        return false;
-    }
-};
-
-// basic search function of repo object values
-const searchFilter = function (searchValue) {
-    return repo.filter(function (country) {
-        let nameMatch = searchValue === country.name,
-            regionMatch = searchValue === country.region,
-            languageMatch = country.languages.includes(searchValue);
-
-        if (nameMatch || regionMatch || languageMatch) {
-            return true;
-        } else {
-            return false;
-        }
-    });
-};
-
-// returns search result to console
-const searchResult = function (searchValue) {
-    let filterResult = searchFilter(searchValue);
-
-    if (filterResult.length === 0) {
-        console.log(`${searchValue} cannot be found`);
-        return false;
-    } else {
-        console.log(filterResult);
-        return true;
-    }
+            add(nation);
+            console.log(nation);
+        });
+    }).catch(function (error) {
+        console.error(error);
+    })
 }
 
-// organises country data by region
-// potentially use to separate each section on webpage
-const regionalOverview = repo.reduce(function (regionSummary, country) {
-    regionSummary[country.region] = regionSummary[country.region] || [];
-    regionSummary[country.region].push({
-        Name: country.name,
-        Languages: country.languages
-    });
-    return regionSummary;
-}, {});
-
-// 
+// displays country name on buttons with click event
 const displayDetails = country => console.log(country);
 
 const displayCountryList = function (country) {
     let countryList = document.querySelector('.country-list'),
-        countryListItem = document.createElement('li'),
-        button = document.createElement('button');
+        countryListItem = document.createElement('li');
 
-    button.textContent = country.name;
+    let countryName = document.createElement('p');
+    countryName.textContent = country.name;
+    countryName.classList.add('country-name-main');
+
+    let countryImage = document.createElement('img');
+    countryImage.src = country.flag;
+    countryImage.classList.add('country-image-main');
+
+    let buttonContainer = document.createElement('div');
+    buttonContainer.classList.add('button-container');
+
+    let button = document.createElement('button');
     button.classList.add('button-main');
+
+    buttonContainer.appendChild(countryName);
+    buttonContainer.appendChild(countryImage);
+    button.appendChild(buttonContainer);
     countryListItem.appendChild(button);
     countryList.appendChild(countryListItem);
 
@@ -102,20 +64,113 @@ const displayCountryList = function (country) {
     });
 };
 
+
+
+loadApiList().then(function () {
+    worldList.forEach(country => displayCountryList(country));
+});
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+// OLD CODE
+
+// const repo = [
+//     { name: 'Sweden', region: 'Europe', languages: 'Swedish' },
+//     { name: 'Vanuatu', region: 'Oceania', languages: ['Bislama', 'English', 'French'] },
+//     { name: 'Samoa', region: 'Oceania', languages: ['Samoan', 'English'] },
+//     { name: 'Saint Vincent and the Grenadines', region: 'Americas', languages: 'English' },
+//     { name: 'Republic of Korea', region: 'Asia', languages: 'Korean' }
+// ];
+
+// const repoCategories = ['name', 'region', 'languages'];
+
+// // checks categories of new country match categories in repo
+// const checkCategories = function (newCountry) {
+//     let result = true;
+//     Object.keys(newCountry).forEach(function (category) {
+//         if (!repoCategories.includes(category)) {
+//             console.log(`${newCountry.name} has the invalid category '${category}'`);
+//             result = false;
+//         }
+//     });
+//     return result;
+// };
+
+// // checks new country is a valid object and has the same number of categories as the countries in repo, then moves to checkCategories function
+// const validateNewCountry = function (newCountry) {
+//     if (typeof newCountry === 'object' && newCountry !== null &&
+//         Object.keys(newCountry).length === repoCategories.length) {
+//         return checkCategories(newCountry);
+//     } else {
+//         console.log(`${JSON.stringify(newCountry)} is not a valid object or does not have the correct categories`);
+//         return false;
+//     }
+// };
+
+// // adds new country to repo if it passes validation checks
+// const add = function (newCountry) {
+//     if (validateNewCountry(newCountry)) {
+//         return repo.push(newCountry);
+//     } else {
+//         console.log(`${newCountry.name || newCountry} has not been added`);
+//         return false;
+//     }
+// };
+
+// basic search function of repo object values
+// const searchFilter = function (searchValue) {
+//     return worldList.filter(function (country) {
+//         let nameMatch = searchValue === country.name,
+//             regionMatch = searchValue === country.region,
+//             languageMatch = country.languages.includes(searchValue);
+
+//         if (nameMatch || regionMatch || languageMatch) {
+//             return true;
+//         } else {
+//             return false;
+//         }
+//     });
+// };
+
+// // returns search result to console
+// const searchResult = function (searchValue) {
+//     let filterResult = searchFilter(searchValue);
+
+//     if (filterResult.length === 0) {
+//         console.log(`${searchValue} cannot be found`);
+//         return false;
+//     } else {
+//         console.log(filterResult);
+//         return true;
+//     }
+// }
+
+// // organises country data by region
+// // potentially use to separate each section on webpage
+// const regionalOverview = worldList.reduce(function (regionSummary, country) {
+//     regionSummary[country.region] = regionSummary[country.region] || [];
+//     regionSummary[country.region].push({
+//         Name: country.name,
+//         Languages: country.languages
+//     });
+//     return regionSummary;
+// }, {});
+
 //TESTS
-add({ name: 'Greenland', region: 'Americas', languages: 'Kalaallisut' });
-add('Atlantis');
-add({ name: 'Saturn', region: 'Space' });
-add({ name: 'Jupiter', region: 'Space', moons: 79 });
-console.log(repo);
+// add({ name: 'Greenland', region: 'Americas', languages: 'Kalaallisut' });
+// add('Atlantis');
+// add({ name: 'Saturn', region: 'Space' });
+// add({ name: 'Jupiter', region: 'Space', moons: 79 });
+// console.log(repo);
 
-searchResult('Jupiter');
-searchResult('English');
-searchResult('Oceania');
+// searchResult('Jupiter');
+// searchResult('English');
+// searchResult('Oceania');
 
-console.log(regionalOverview);
+// console.log(regionalOverview);
 //END TESTS
 
-repo.forEach(function (country) {
-    displayCountryList(country);
-});
+// repo.forEach(function (country) {
+//     displayCountryList(country);
+// });

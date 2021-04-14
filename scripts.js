c7773ab15a94f2f496545b46833d1cf00efd741d
Hyun Young Lee <hyunyounglee@Hyuns-MacBook-Pro.local>
2021-04-14 12:19:36 +0100

    Add helper functions for modal DOM manipulation
    - helper functions to reduce repetition of creating elements and adding them to the DOM
    - updates to variable names
    
    with '#' will be ignored, and an empty message aborts the commit.

--- scripts.js
+++ scripts.js
@@ -1,15 +1,15 @@
 let worldList = [];
 
-const apiUrl = 'https://restcountries.eu/rest/v2/';
+const countryApiUrl = 'https://restcountries.eu/rest/v2/';
 const modalContainer = document.querySelector('.modal-container');
 const loadingSpinner = document.querySelector('.loader');
 
 const showLoadingSpinner = () => loadingSpinner.hidden = false;
 const hideLoadingSpinner = () => loadingSpinner.hidden = true;
 
-const loadApiList = function () {
+const loadCountryApi = function () {
     showLoadingSpinner();
-    return fetch(apiUrl).then(function (response) {
+    return fetch(countryApiUrl).then(function (response) {
         return response.json();
     }).then(function (data) {
         worldList = data.map(function (country) {
@@ -36,69 +36,70 @@ const loadApiList = function () {
     });
 };
 
+const newDomElement = element => document.createElement(element);
+
 // creates modal
 const showModal = function (country) {
     modalContainer.innerHTML = '';
-    let modal = document.createElement('div');
+    let modal = newDomElement('div');
     modal.classList.add('modal');
 
-    let modalCloseButton = document.createElement('button');
+    let modalCloseButton = newDomElement('button');
     modalCloseButton.classList.add('modal-close-button');
     modalCloseButton.textContent = 'X';
     modalCloseButton.addEventListener('click', closeModal);
 
-    let countryName = document.createElement('h1');
-    countryName.classList.add('country-name');
-    countryName.textContent = country.name;
+    let modalHeading = newDomElement('h1');
+    modalHeading.classList.add('modal-heading');
+    modalHeading.textContent = country.name;
 
-    let countryMapContainer = document.createElement('div');
-    countryMapContainer.classList.add('country-map-container');
+    let modalMapContainer = newDomElement('div');
+    modalMapContainer.classList.add('modal-map-container');
 
-    let countryMap = document.createElement('iframe');
+    let countryMap = newDomElement('iframe');
     countryMap.src = `https://www.google.com/maps/embed/v1/place?key=AIzaSyAbg-EXRXLkHNQQnZ--E45nvlUmdY75A1A&q=${country.name}`;
     countryMap.classList.add('country-map');
 
-    let modalTextContainer = document.createElement('div');
+    let modalTextContainer = newDomElement('div');
     modalTextContainer.classList.add('modal-text-container');
 
-    let countryRegion = document.createElement('p');
-    countryRegion.innerHTML = `<span class="modal-subheading">Region:</span> ${country.region}`;
+    let countryDetailsTemplate = (subheading, countryProperty) => `${subheading}: ${countryProperty}`;
 
-    let countryCapital = document.createElement('p');
-    countryCapital.innerHTML = `<span class="modal-subheading">Capital:</span> ${country.capital}`;
+    let countryRegion = newDomElement('p');
+    countryRegion.innerHTML = countryDetailsTemplate('Region', country.region);
 
-    let countryArea = document.createElement('p');
-    countryArea.innerHTML = `<span class="modal-subheading">Area:</span> ${country.area}km`;
+    let countryCapital = newDomElement('p');
+    countryCapital.innerHTML = countryDetailsTemplate('Capital', country.capital);
 
-    let countryPopulation = document.createElement('p');
-    countryPopulation.innerHTML = `<span class="modal-subheading">Population:</span> ${country.population}`;
+    let countryArea = newDomElement('p');
+    let countryAreaFormatted = `${country.area.toLocaleString()}km`;
+    countryArea.innerHTML = countryDetailsTemplate('Area', countryAreaFormatted);
 
-    let countryLanguages = document.createElement('p');
+    let countryPopulation = newDomElement('p');
+    let countryPopulationFormatted = `${country.population.toLocaleString()}`;
+    countryPopulation.innerHTML = countryDetailsTemplate('Population', countryPopulationFormatted);
+
+    let countryLanguages = newDomElement('p');
     let countryLanguagesFormatted = country.languages.join(', ');
-    countryLanguages.innerHTML = `<span class="modal-subheading">Languages:</span> ${countryLanguagesFormatted}`;
+    countryLanguages.innerHTML = countryDetailsTemplate('Languages', countryLanguagesFormatted);
 
-    let countryCurrencies = document.createElement('p');
-    countryCurrencies.innerHTML = `<span class="modal-subheading">Currencies:</span> ${country.currencies}`;
+    let countryCurrencies = newDomElement('p');
+    countryCurrencies.innerHTML = countryDetailsTemplate('Currencies', country.currencies);
 
-    let countryNeighbours = document.createElement('p');
-    let countryNeighboursFormatted = country.borders.join(', ');
-    countryNeighbours.innerHTML = `<span class="modal-subheading">Borders with</span> ${countryNeighboursFormatted}`;
-
-    let capitalTimezone = document.createElement('p');
-    capitalTimezone.classList.add('capital-current-time');
-    capitalTimezone.innerHTML = `<span class="modal-subheading">Current time in ${country.capital}:</span> placeholder`;
+    let capitalTimezone = newDomElement('p');
+    capitalTimezone.classList.add('modal-current-time');
+    capitalTimezone.innerHTML = `Current time in ${country.capital}: placeholder`;
 
     modal.appendChild(modalCloseButton);
-    modal.appendChild(countryName);
-    countryMapContainer.appendChild(countryMap);
-    modal.appendChild(countryMapContainer);
+    modal.appendChild(modalHeading);
+    modalMapContainer.appendChild(countryMap);
+    modal.appendChild(modalMapContainer);
     modalTextContainer.appendChild(countryRegion);
     modalTextContainer.appendChild(countryCapital);
     modalTextContainer.appendChild(countryArea);
     modalTextContainer.appendChild(countryPopulation);
     modalTextContainer.appendChild(countryLanguages);
     modalTextContainer.appendChild(countryCurrencies);
-    modalTextContainer.appendChild(countryNeighbours);
     modal.appendChild(modalTextContainer);
     modal.appendChild(capitalTimezone);
     modalContainer.appendChild(modal);
@@ -113,6 +114,9 @@ const displayCountryList = function (country) {
     let countryButton = document.createElement('button');
     countryButton.classList.add('country-button');
 
+    let imageContainer = document.createElement('div');
+    imageContainer.classList.add('image-container-main');
+
     let countryImage = document.createElement('img');
     countryImage.src = country.flag;
     countryImage.classList.add('country-image-main');
@@ -121,7 +125,8 @@ const displayCountryList = function (country) {
     countryName.textContent = country.name;
     countryName.classList.add('country-name-main');
 
-    countryButton.appendChild(countryImage);
+    imageContainer.appendChild(countryImage);
+    countryButton.appendChild(imageContainer);
     countryButton.appendChild(countryName);
     countryList.appendChild(countryButton);
 
@@ -132,7 +137,7 @@ const displayCountryList = function (country) {
 
 // displays details in a modal
 const displayDetails = country =>
-    loadApiList(country).then(() => showModal(country));
+    loadCountryApi(country).then(() => showModal(country));
 
 // closes modal
 const closeModal = () => modalContainer.classList.remove('is-visible');
@@ -149,111 +154,7 @@ document.addEventListener('keydown', event => {
     };
 });
 
-loadApiList().then(function () {
+loadCountryApi().then(function () {
     worldList.forEach(country => displayCountryList(country));
 });
 
-
-/////////////////////////////////////////////////////////////////////////////////////////////
-
-// OLD CODE
-
-// const repo = [
-//     { name: 'Sweden', region: 'Europe', languages: 'Swedish' },
-//     { name: 'Vanuatu', region: 'Oceania', languages: ['Bislama', 'English', 'French'] },
-//     { name: 'Samoa', region: 'Oceania', languages: ['Samoan', 'English'] },
-//     { name: 'Saint Vincent and the Grenadines', region: 'Americas', languages: 'English' },
-//     { name: 'Republic of Korea', region: 'Asia', languages: 'Korean' }
-// ];
-
-// const repoCategories = ['name', 'region', 'languages'];
-
-// // checks categories of new country match categories in repo
-// const checkCategories = function (newCountry) {
-//     let result = true;
-//     Object.keys(newCountry).forEach(function (category) {
-//         if (!repoCategories.includes(category)) {
-//             console.log(`${newCountry.name} has the invalid category '${category}'`);
-//             result = false;
-//         }
-//     });
-//     return result;
-// };
-
-// // checks new country is a valid object and has the same number of categories as the countries in repo, then moves to checkCategories function
-// const validateNewCountry = function (newCountry) {
-//     if (typeof newCountry === 'object' && newCountry !== null &&
-//         Object.keys(newCountry).length === repoCategories.length) {
-//         return checkCategories(newCountry);
-//     } else {
-//         console.log(`${JSON.stringify(newCountry)} is not a valid object or does not have the correct categories`);
-//         return false;
-//     }
-// };
-
-// // adds new country to repo if it passes validation checks
-// const add = function (newCountry) {
-//     if (validateNewCountry(newCountry)) {
-//         return repo.push(newCountry);
-//     } else {
-//         console.log(`${newCountry.name || newCountry} has not been added`);
-//         return false;
-//     }
-// };
-
-// basic search function of repo object values
-// const searchFilter = function (searchValue) {
-//     return worldList.filter(function (country) {
-//         let nameMatch = searchValue === country.name,
-//             regionMatch = searchValue === country.region,
-//             languageMatch = country.languages.includes(searchValue);
-
-//         if (nameMatch || regionMatch || languageMatch) {
-//             return true;
-//         } else {
-//             return false;
-//         }
-//     });
-// };
-
-// // returns search result to console
-// const searchResult = function (searchValue) {
-//     let filterResult = searchFilter(searchValue);
-
-//     if (filterResult.length === 0) {
-//         console.log(`${searchValue} cannot be found`);
-//         return false;
-//     } else {
-//         console.log(filterResult);
-//         return true;
-//     }
-// }
-
-// // organises country data by region
-// // potentially use to separate each section on webpage
-// const regionalOverview = worldList.reduce(function (regionSummary, country) {
-//     regionSummary[country.region] = regionSummary[country.region] || [];
-//     regionSummary[country.region].push({
-//         Name: country.name,
-//         Languages: country.languages
-//     });
-//     return regionSummary;
-// }, {});
-
-//TESTS
-// add({ name: 'Greenland', region: 'Americas', languages: 'Kalaallisut' });
-// add('Atlantis');
-// add({ name: 'Saturn', region: 'Space' });
-// add({ name: 'Jupiter', region: 'Space', moons: 79 });
-// console.log(repo);
-
-// searchResult('Jupiter');
-// searchResult('English');
-// searchResult('Oceania');
-
-// console.log(regionalOverview);
-//END TESTS
-
-// repo.forEach(function (country) {
-//     displayCountryList(country);
-// });
